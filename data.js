2a3ee1c6f72f8a2f17a645a849e65fa5c959930e
blaady-baldy <devanshsingh201001@gmail.com>
2022-03-22 10:59:22 +0530

    Adding audio effect

---
+++ data.js
@@ -0,0 +1,8 @@
+var data = {
+	q: {
+		sound: new Howl({
+  		urls: ['assets/sounds/rainfall1.mp4']
+		}),
+		color: '#1abc9c'
+	}
+}
--- scripts.js
+++ scripts.js
@@ -1,5 +1,19 @@
 const menuToggle = document.querySelector('.toggle');
 const showcase = document.querySelector('.showcase');
+const play_audio = document.querySelector('.audio');
+var audio = new Audio("/assets/sounds/rain-01.wav");
+
+play_audio.onclick = function() {
+  audio.play();
+}
+
+
+// function onKeyDown(event) {
+//   if(keyData[event.key]){
+//     keyData[event.key].sound.play();
+//   }
+//   console.log(event.key);
+// }
 
 menuToggle.addEventListener('click', () => {
   menuToggle.classList.toggle('active');
